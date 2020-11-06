82eeb040bd651efb4551d1fe3d0066ce28fe1731
Eileen Rhodes <etrhodes619@gmail.com>
2020-11-06 15:16:00 -0700

    updates

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-const apiKey = 'XJ5A8I8b0ezCdeJuK9ajOJxQh1rN8eXuxjAgusCr'
-const searchURL = 'https://developer.nps.gov/api/v1/parks?key=XJ5A8I8b0ezCdeJuK9ajOJxQh1rN8eXuxjAgusCr'
+const apiKey = 'XJ5A8I8b0ezCdeJuK9ajOJxQh1rN8eXuxjAgusCr';
+var searchURL = `https://developer.nps.gov/api/v1/parks?`;
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
-    .map(key => )
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+    console.log(queryItems);
+    console.log(queryItems.join('&'));
+    return queryItems.join('&');
 }
 
 function getNationalParks(state, limit=10) {
     const params = {
-        key: apiKey,
+        api_key: apiKey,
         stateCode: state,
         limit,
     }
+    let queryString = formatQueryParams(params);
+    searchURL += queryString;
+    fetch(searchURL) 
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+        })
+        .then(responseJson => 
+            displayResults(responseJson)
+        );
 }
-
 
 function onSubmit() {
     $('form').on('submit', event => {
         event.preventDefault();
-
-
-        $('#submit').removeClass('hidden');
+        let state = $('.state-name').val();
+        let limit = $('.number').val();
+        getNationalParks(state, limit);
     })
 }
 
-function displayResults() {
-    $('#results').empty();
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#target').empty();
+    let i = 0;
+    for (let i = 0; i < responseJson.length; i++)
+    $('#target').append(`
+    <li>
+        <p>${responseJson.data[i].fullName}</p>
+        <p>${responseJson.data[i].description}</p>
+    </li>
+    `)
+    $('#results').removeClass('hidden');
 }
- 
+
 function handler() {
     onSubmit();
 }
+
+handler();
